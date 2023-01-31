81fb62a16789d2f51a54f27a7569dc95f4fee0f7
stevescilar <stevemuambi@gmail.com>
2023-01-31 16:46:21 +0300

    done with Navbar-onto-header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,47 @@
 import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
-import { MdOutLineRestaurantMenu } from 'react-icons/md'
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
 import images from '../../constants/images'
 import './Navbar.css';
 
-const  Navbar = () => (
-  <nav className='app__navbar'>
-    <div className="app__navbar-logo">
-      <img src={images.gericht} alt="app logo" />
-    </div>
-    <ul className='app__navbar-links'>
-      <li className='p__opensans'><a href="#home">Home</a></li>
-      <li className='p__opensans'><a href="#About">About</a></li>
-      <li className='p__opensans'><a href="#Menu">Menu</a></li>
-      <li className='p__opensans'><a href="#Awards">Awards</a></li>
-      <li className='p__opensans'><a href="#Contact">Contacts</a></li>
-    </ul>
-    <div className="app__navbar-login">
-      <a href="#login" className='p__opensans'> Log In / Register </a>
-      <div />
-      <a href="/" className="p__opensans">Book A Table</a>
-    </div>
-  </nav>
-);
+const  Navbar = () => { 
+    const [toggleMenu, setToggleMenu] = React.useState(false);
+  return ( 
+      <nav className='app__navbar'>
+        <div className="app__navbar-logo">
+          <img src={images.gericht} alt="app logo" />
+        </div>
+        <ul className='app__navbar-links'>
+          <li className='p__opensans'><a href="#home">Home</a></li>
+          <li className='p__opensans'><a href="#About">About</a></li>
+          <li className='p__opensans'><a href="#Menu">Menu</a></li>
+          <li className='p__opensans'><a href="#Awards">Awards</a></li>
+          <li className='p__opensans'><a href="#Contact">Contacts</a></li>
+        </ul>
+        <div className="app__navbar-login">
+          <a href="#login" className='p__opensans'> Log In / Register </a>
+          {/* below div for a bar between login and register */}
+          <div />  
+          <a href="/" className="p__opensans">Book A Table</a>
+        </div>
+        <div className="app__navbar-smallscreen">
+          <GiHamburgerMenu  color='#fff' fontSize={27} onClick={() => setToggleMenu(true)} />
 
+         { toggleMenu && ( 
+            <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+              <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+              <ul className='app__navbar-smallscreen_links'>
+              <li className='p__opensans'><a href="#home">Home</a></li>
+              <li className='p__opensans'><a href="#About">About</a></li>
+              <li className='p__opensans'><a href="#Menu">Menu</a></li>
+              <li className='p__opensans'><a href="#Awards">Awards</a></li>
+              <li className='p__opensans'><a href="#Contact">Contacts</a></li>
+            </ul>
+            </div>
+          ) }
+        </div>
+      </nav>
+    );
+  }
 export default Navbar;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { SubHeading } from '../../components';
 
+import {images} from '../../constants';
 import './Header.css';
 
+
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="Home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Lets Meet while we eat meat"/>
+      <h1 className="app__header-h1">
+        Kamakis' is the Joint you've been missing
+      </h1>
+      <p className="p__opensans" style={{ margin: '2rem 0'}}>
+      The flavors are expertly balanced, with every bite exploding with richness and depth. The tender, juicy meat pairs perfectly with the crispy, aromatic vegetables, and the sauce is like a symphony of spices on the tongue. I'm blown away by the skill of the chef, and I can tell that each ingredient has been carefully selected and prepared with care. This is truly the epitome of delicious food, and I feel so grateful to be able to enjoy it.
+      </p>
+      <button type='button' className='custom__button'>Look at the Menu</button>
+    </div>
+
+    <div className="app__wrapper_img">
+
+    </div>
   </div>
 );
 
