bfcd6188810df2ebace9ca5830a6bbbc5f38c9f0
stevescilar <stevemuambi@gmail.com>
2023-01-31 17:13:58 +0300

    header section done

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -13,13 +13,13 @@ const Header = () => (
         Kamakis' is the Joint you've been missing
       </h1>
       <p className="p__opensans" style={{ margin: '2rem 0'}}>
-      The flavors are expertly balanced, with every bite exploding with richness and depth. The tender, juicy meat pairs perfectly with the crispy, aromatic vegetables, and the sauce is like a symphony of spices on the tongue. I'm blown away by the skill of the chef, and I can tell that each ingredient has been carefully selected and prepared with care. This is truly the epitome of delicious food, and I feel so grateful to be able to enjoy it.
+      The flavors are expertly balanced, with every bite exploding with richness and depth. The tender, juicy meat pairs perfectly with the crispy, aromatic vegetables, and the sauce is like a symphony of spices on the tongue.
       </p>
       <button type='button' className='custom__button'>Look at the Menu</button>
     </div>
 
     <div className="app__wrapper_img">
-
+      <img src={images.welcome} alt="greatimage" />
     </div>
   </div>
 );
