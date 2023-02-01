f738bfbd3215e993f461a6c97f1b7601bc8a8818
stevescilar <stevemuambi@gmail.com>
2023-02-01 15:56:56 +0300

    Chefs' section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="mpishi" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">World Class Mixologist, Calories measured with precision</p>
+        </div>
+        <p className="p__opensans">The Tender, Juicy Meat Pairs Perfectly With The Crispy, Aromatic Vegetables, And The Sauce Is Like A Symphony Of Spices On The Tongue.</p>
+      </div>
+      <div className="app__chef-sign">
+        <p>Stephen Muambi</p>
+        <p className="p__opensans">Q & A Officer</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
